63057fb54dbd2b3543524c7ce8656a4ba9a247cc
ayajnik <ayush.yajnik@yahoo.com>
2025-04-01 14:19:12 -0400

    adding langchain google genai

--- src/lapses_ai/crew.py
+++ src/lapses_ai/crew.py
@@ -1,5 +1,7 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
